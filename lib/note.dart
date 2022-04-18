7f205ab02ec4791b4ba851797c1dc0784ea074d4
paulsump <sumpner@hotmail.com>
2022-04-18 09:48:21 +0100

    trim 3 of 'c'

--- lib/note.dart
+++ lib/note.dart
@@ -20,11 +20,11 @@ class Note {
 }
 
 const letters = [
-  'a',
-  'ab',
-  'b',
-  'bb',
-  'c',
+  // 'a',
+  // 'ab',
+  // 'b',
+  // 'bb',
+  // 'c',
   // 'd',
   // 'db',
   // 'e',
@@ -34,8 +34,8 @@ const letters = [
   // 'gb'
 ];
 
-const octaves = [1, 2, 3];
-// const octaves = [1, 2, 3, 4, 5, 6];
+// const octaves = [1, 2, 3];
+const octaves = [1, 2, 3, 4, 5, 6];
 // TODO octave 0 & 7 is missing some letters
 // const octaves = [0, 1, 2, 3, 4, 5, 6, 7];
 
