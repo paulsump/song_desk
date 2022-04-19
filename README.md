c572dec540c10498c937895659ef26022b8ba937
paulsump <sumpner@hotmail.com>
2022-04-19 15:44:17 +0100

    Moved _audioCache out to file space so i can split up Notes class.

--- README.md
+++ README.md
@@ -10,7 +10,8 @@
 - Stop button play next pause
 - store last played song
 - bassline?
-
+- note duration
+    Never Leave Me Lonely
 - BAss
 -   Enjoy the Silence
 
@@ -19,3 +20,4 @@
 
 
 - song tempo - would want save? - perhaps do in separate file.
+- piano octave 0 & 7 is missing some letters
--- lib/player/note.dart
+++ lib/player/note.dart
@@ -4,6 +4,7 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:song_desk/out.dart';
 
 const noWarn = out;
+final _audioCache = AudioCache();
 
 /// play a note
 class Note {
@@ -19,36 +20,33 @@ class Note {
   final AudioPlayer audioPlayer;
 }
 
-const letters = [
-  'c',
-  'db',
-  'd',
-  'eb',
-  'e',
-  'f',
-  'gb',
-  'g',
-  'ab',
-  'a',
-  'bb',
-  'b',
-];
-
-const octaves = [1, 2, 3, 4, 5, 6];
-// TODO octave 0 & 7 is missing some letters
-// const octaves = [0, 1, 2, 3, 4, 5, 6, 7];
-
 /// loads and plays notes.
 /// TODO extract PianoNotes from Player
 class Notes {
   final list = <Note>[];
 
   late AudioPlayer kickAudioPlayer;
-  final audioCache = AudioCache();
 
   Future<void> preLoad() async {
     kickAudioPlayer = await _createAudioPlayer('kick.wav');
     kickAudioPlayer.setVolume(0.3);
+
+    const letters = [
+      'c',
+      'db',
+      'd',
+      'eb',
+      'e',
+      'f',
+      'gb',
+      'g',
+      'ab',
+      'a',
+      'bb',
+      'b',
+    ];
+
+    const octaves = [1, 2, 3, 4, 5, 6];
 
     for (final octave in octaves) {
       for (final letter in letters) {
@@ -59,22 +57,21 @@ class Notes {
           octave: octave,
           audioPlayer: await _createAudioPlayer(fileName),
         ));
-
       }
     }
   }
+}
 
-  Future<AudioPlayer> _createAudioPlayer(String fileName) async {
-    final url = await audioCache.load(fileName);
+Future<AudioPlayer> _createAudioPlayer(String fileName) async {
+  final url = await _audioCache.load(fileName);
 
-    // LOW_LATENCY seems to be needed to replay
-    final audioPlayer = AudioPlayer(mode: PlayerMode.LOW_LATENCY);
+  // LOW_LATENCY seems to be needed to replay
+  final audioPlayer = AudioPlayer(mode: PlayerMode.LOW_LATENCY);
 
-    // prepare the player with this audio but do not start playing
-    unawaited(audioPlayer.setUrl(url.path));
+  // prepare the player with this audio but do not start playing
+  unawaited(audioPlayer.setUrl(url.path));
 
-    // set release mode so that it never releases (I can't hear effect though currently)
-    unawaited(audioPlayer.setReleaseMode(ReleaseMode.STOP));
-    return audioPlayer;
-  }
+  // set release mode so that it never releases (I can't hear effect though currently)
+  unawaited(audioPlayer.setReleaseMode(ReleaseMode.STOP));
+  return audioPlayer;
 }
