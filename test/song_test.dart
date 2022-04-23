dfa2c6763ad15e467fd4f180e752dd65d139faf1
paulsump <sumpner@hotmail.com>
2022-04-23 10:33:48 +0100

    added (failing) calcDuration() test.

--- test/song_test.dart
+++ test/song_test.dart
@@ -5,6 +5,7 @@ import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:song_desk/loader/persist.dart';
 import 'package:song_desk/loader/song.dart';
+import 'package:song_desk/player/calc_duration.dart';
 
 /// Test [Song] functions.
 void main() {
@@ -21,13 +22,16 @@ void main() {
     });
   });
 
-  group('f', () {
-    test('C', () async {
+  group('calcDuration', () {
+    test('1', () async {
       final persist = Persist();
       const title = 'After All';
 
       await persist.loadSong(title);
       final song = persist.songs[title]!;
+
+      final int duration = calcDuration(16, 1, 'bass', song.bars);
+      expect(duration, equals(1));
     });
   });
 }
